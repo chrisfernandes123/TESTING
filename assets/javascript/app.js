cabf115c2729481ee2390ba48f29faff26919cc0
papasnip <chrisfernandes123@hotmail.com>
2018-11-19 19:34:48 -0500

    201811191934

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,7 +16,9 @@ console.log("start");
 
         //var queryURL = "https://maxim75-geolocation-ws-v1.p.mashape.com/api/find_box?e=31&lang=en&n=51&s=50&w=30&wiki_commons=0";
 
-        var queryURL = "https://montanaflynn-Airports-v1.p.mashape.com"
+        //var queryURL = "https://montanaflynn-Airports-v1.p.mashape.com"
+
+        var queryURL = "https://jgentes-Crime-Data-v1.p.mashape.com/crime?enddate=9%2F25%2F2015&lat=37.757815&long=-122.5076392&startdate=9%2F19%2F2015"
 
 
         $.ajax({
@@ -26,12 +28,12 @@ console.log("start");
                 // "x-mashape-key": "c49VVibm9ImshDElgqf7QTzDFVoXp1v5VX2jsnWndwn7Phj8ic",
                 //   "accept": "application/json"
                 // "X-Mashape-Key": "c49VVibm9ImshDElgqf7QTzDFVoXp1v5VX2jsnWndwn7Phj8ic"
+            
+               // "X-Mashape-Authorization": "c49VVibm9ImshDElgqf7QTzDFVoXp1v5VX2jsnWndwn7Phj8ic"
+                //,                "Accept": "application/json"
 
-             
-                "X-Mashape-Key": "c49VVibm9ImshDElgqf7QTzDFVoXp1v5VX2jsnWndwn7Phj8ic",
+               "X-Mashape-Key": "c49VVibm9ImshDElgqf7QTzDFVoXp1v5VX2jsnWndwn7Phj8ic",
                 "Accept": "application/json"
-
-
 
 
             }
